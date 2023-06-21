3f33b08231449c789a9d8b1d2940a0c0e08de4c3
KacperJanczyk220646 <112700204+KacperJanczyk220646@users.noreply.github.com>
2023-06-21 03:03:52 +0200

    updated

---
+++ 2_Second.py
@@ -0,0 +1,27 @@
+"""
+Introduction Page for GreenScape Project in Streamlit
+
+This script creates a Streamlit web page for the GreenScape project, which aims to improve the Green score in Breda. The page contains 5 multiple tabs, each displaying different information related to the project.
+
+Usage:
+    - Run the script to launch the introduction page in Streamlit.
+
+Dependencies:
+    - streamlit
+    - PIL (Python Imaging Library)
+
+"""
+
+import streamlit as st
+from PIL import Image
+
+
+def main():
+    """
+    Main function to create the Streamlit page for the GreenScape project.
+    """
+    st.title("Second page")
+
+
+if __name__ == "__main__":
+    main()
